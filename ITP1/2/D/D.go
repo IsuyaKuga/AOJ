3ac8e4c2d28fcad5e567dab41b3743e00e4ce1ff
Gaku Yasui <isuya.kuga@gmail.com>
2019-04-20 17:49:19 +0000

    Added ITP1/2/D/D.go

--- ITP1/2/D/D.go
+++ ITP1/2/D/D.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-  "bufio"
-  "os"
-  "fmt"
-  "strings"
-  "strconv"
+	. "fmt"
 )
 
-func main () {
-  stdin := bufio.NewScanner(os.Stdin)
-  stdin.Scan()
-  inputs := strings.Fields(stdin.Text())
-  W, _ := strconv.Atoi(inputs[0])
-  H, _ := strconv.Atoi(inputs[1])
-  x, _ := strconv.Atoi(inputs[2])
-  y, _ := strconv.Atoi(inputs[3])
-  r, _ := strconv.Atoi(inputs[4])
-
-  if (0 <= x-r) && (x+r <= W) && (0 <= y-r) && (y+r <= H) {
-    fmt.Println("Yes")
-  } else {
-    fmt.Println("No")
-  }
+func main() {
+	var W, H, x, y, r int
+	Scan(&W, &H, &x, &y, &r)
+	if (0 <= x-r) && (x+r <= W) && (0 <= y-r) && (y+r <= H) {
+		Println("Yes")
+	} else {
+		Println("No")
+	}
 }
