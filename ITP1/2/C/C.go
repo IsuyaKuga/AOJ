01a6994b0a08502ca809520dedba05bddbaf5e3c
Gaku Yasui <isuya.kuga@gmail.com>
2019-04-21 17:51:53 +0000

    Added ITP1/2/C/C.go

--- ITP1/2/C/C.go
+++ ITP1/2/C/C.go
@@ -1,34 +1,20 @@
 package main
 
 import (
-  "bufio"
-  "os"
-  "fmt"
-  "strings"
-  "strconv"
+	. "fmt"
 )
 
-func swap(x, y int) (int, int) {
-  return y, x
-}
-
 func main() {
-  stdin := bufio.NewScanner(os.Stdin)
-  stdin.Scan()
-  nums := strings.Fields(stdin.Text())
-  a, _ := strconv.Atoi(nums[0])
-  b, _ := strconv.Atoi(nums[1])
-  c, _ := strconv.Atoi(nums[2])
-
-  if c < b {
-    b, c = swap(b, c)
-  }
-  if b < a {
-    a, b = swap(a, b)
-  }
-  if c < b {
-    b, c = swap(b, c)
-  }
-
-  fmt.Println(a,b,c)
+	var a, b, c int
+	Scan(&a, &b, &c)
+	if c < b {
+		b, c = c, b
+	}
+	if b < a {
+		a, b = b, a
+	}
+	if c < b {
+		b, c = c, b
+	}
+	Println(a, b, c)
 }
